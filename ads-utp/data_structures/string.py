f6f23bc8d8fb5cd68090e8afdde4fd74eaa34af1
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2025-04-06 11:32:42 +0300

    linked list implementation & tests

---
+++ ads-utp/data_structures/string.py
@@ -0,0 +1,59 @@
+def string_length(s):
+    """Return the length of the string.
+
+    Args:
+        s (string): The string to be measured.
+
+    Returns:
+        int: The length of the string.
+    """
+    return len(s)
+
+
+def string_concat(s1, s2):
+    """Concatenate two strings.
+
+    Args:
+        s1 (string): The first string.
+        s2 (string): The second string.
+
+    Returns:
+        string: The concatenated string.
+    """
+    return s1 + s2
+
+
+def string_to_lower(s):
+    """Convert a string to lowercase.
+
+    Args:
+        s (string): The string to be converted.
+
+    Returns:
+        string: The string in lowercase.
+    """
+    return s.lower()
+
+
+def string_to_upper(s):
+    """Convert a string to uppercase.
+
+    Args:
+        s (string): The string to be converted.
+
+    Returns:
+        string: The string in uppercase.
+    """
+    return s.upper()
+
+
+def string_swap_case(s):
+    """Swap the case of a string.
+
+    Args:
+        s (string): The string to be converted.
+
+    Returns:
+        string: The string with swapped case.
+    """
+    return s.swapcase()
