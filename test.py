5633e621f1b9333e944cf9d9f49350ce7567b6d0
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2025-09-14 11:22:10 +0300

    added test case module

---
+++ test.py
@@ -0,0 +1,15 @@
+# Import Data Structures Modules
+from data_structures.string.basic_string import *
+from data_structures.string.string_indexing import *
+from data_structures.string.transform_string import *
+from data_structures.stack.basic_stack import *
+from data_structures.queue.basic_queue import *
+from data_structures.set.basic_set import *
+from data_structures.set.set_operations import *
+from data_structures.list.basic_list import *
+from data_structures.list.list_indexing import *
+from data_structures.list.list_transform import *
+from data_structures.dictionary.basic_dictionary import *
+from data_structures.linked_list.basic_linked_list import *
+
+# You can add test cases here to test the functionalities of the imported modules.
