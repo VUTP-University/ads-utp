8145f2e6c7c7db75f4db45378e4be7991dcaf6e1
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2025-09-14 11:41:41 +0300

    added BST class and methods

--- test.py
+++ test.py
@@ -6,10 +6,14 @@ from data_structures.stack.basic_stack import *
 from data_structures.queue.basic_queue import *
 from data_structures.set.basic_set import *
 from data_structures.set.set_operations import *
+from data_structures.tuple.basic_tuple import *
 from data_structures.list.basic_list import *
 from data_structures.list.list_indexing import *
 from data_structures.list.list_transform import *
 from data_structures.dictionary.basic_dictionary import *
 from data_structures.linked_list.basic_linked_list import *
+
+# Import Binary Search Tree Modules
+from binary_tree.binary_search_tree import BST
 
 # You can add test cases here to test the functionalities of the imported modules.
